fdd38e62c096ae47c80867bb677b99b801c7fa7b
omeratik <fb.omeratik@hotmail.com>
2023-08-15 00:12:43 +0300

    Add project files.

---
+++ Dama/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dama
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+	internal interface ICustomerDal
+	{
+		void Add();
+		void Update();
+		void Delete();
+	}
+
+	class SqlServerCustomerDal : ICustomerDal
+	{
+		public void Add()
+		{
+            Console.WriteLine("SQL added!");
+        }
+
+		public void Delete()
+		{
+            Console.WriteLine("SQL deleted !");
+        }
+
+		public void Update()
+		{
+            Console.WriteLine("SQL updated");
+        }
+	}
+	class OracleCustomerDal : ICustomerDal
+	{
+		public void Add()
+		{
+            Console.WriteLine("Oracle Added "); ;
+		}
+
+		public void Delete()
+		{
+            Console.WriteLine("Oracle deleted"); 
+		}
+
+		public void Update()
+		{
+            Console.WriteLine("Oracle updated");
+		}
+	}
+	class CustomerManager
+	{
+		public void Add(ICustomerDal customerDal)
+		{
+			customerDal.Add();  
+		}
+	}
+}
